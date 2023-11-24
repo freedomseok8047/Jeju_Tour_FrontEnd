7a7eacd14133e4fd5ed4f0b98fc2724dcf30234e
Hyeonseok0627 <stonehead0627@gmail.com>
2023-11-24 20:34:57 +0900

    Community 각 파일구성 완료1_업로드는 실패 문제 발생_해결 필요/auth.xml 누락 부분 추가완료(231124)-학원

---
+++ app/src/main/java/com/example/visit_jeju_app/community/model/CommunityData.kt
@@ -0,0 +1,9 @@
+package com.example.visit_jeju_app.community.model
+
+class CommunityData {
+    var docId: String? = null
+    var title: String? = null
+    var content: String? = null
+    var date: String? = null
+    var comment: String? = null
+}
