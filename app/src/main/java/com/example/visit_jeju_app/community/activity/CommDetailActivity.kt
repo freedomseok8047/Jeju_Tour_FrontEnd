6d971b192b07892d2c160856644e48de01d4d9bf
Hyeonseok0627 <139304184+Hyeonseok0627@users.noreply.github.com>
2023-11-30 02:24:23 +0900

    커뮤니티 작업 중(커뮤니티 관련 뷰 전반적인 변경 및 사진 첨부 작업 진행 중)[231130/02:24]

--- app/src/main/java/com/example/visit_jeju_app/community/activity/CommDetailActivity.kt
+++ app/src/main/java/com/example/visit_jeju_app/community/activity/CommDetailActivity.kt
@@ -4,15 +4,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.example.visit_jeju_app.MyApplication
 import com.example.visit_jeju_app.R
+import com.example.visit_jeju_app.accommodation.AccomActivity
+import com.example.visit_jeju_app.chat.ChatActivity
 import com.example.visit_jeju_app.community.model.CommunityData
 import com.example.visit_jeju_app.community.recycler.CommentAdapter
 import com.example.visit_jeju_app.databinding.ActivityCommDetailBinding
+import com.example.visit_jeju_app.festival.FesActivity
+import com.example.visit_jeju_app.login.AuthActivity
+import com.example.visit_jeju_app.restaurant.ResActivity
+import com.example.visit_jeju_app.shopping.ShopActivity
+import com.example.visit_jeju_app.tour.TourActivity
 import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import java.text.SimpleDateFormat
@@ -31,8 +43,27 @@ class CommDetailActivity : AppCompatActivity() {
         binding = ActivityCommDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //(공통 레이아웃 코드)
+        val headerView = binding.mainDrawerView.getHeaderView(0)
+        val headerUserEmail = headerView.findViewById<TextView>(R.id.headerUserEmail)
+        val headerLogoutBtn = headerView.findViewById<Button>(R.id.headerLogoutBtn)
+
+        headerLogoutBtn.setOnClickListener {
+            // 로그아웃 로직
+            MyApplication.auth.signOut()
+            MyApplication.email = null
+            // 로그아웃 후 처리 (예: 로그인 화면으로 이동)
+            val intent = Intent(this, AuthActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        val userEmail1 = intent.getStringExtra("USER_EMAIL") ?: "No Email"
+        headerUserEmail.text = userEmail1
+
         // 공통 메인 레이아웃 적용 코드
         setSupportActionBar(binding.toolbar)
+
         //드로워화면 액션버튼 클릭 시 드로워 화면 나오게 하기
         toggle =
             ActionBarDrawerToggle(
@@ -42,22 +73,46 @@ class CommDetailActivity : AppCompatActivity() {
                 R.string.open,
                 R.string.close
             )
-
         binding.drawerLayout.addDrawerListener(toggle)
+
         //화면 적용하기
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         //버튼 클릭스 동기화 : 드로워 열어주기
         toggle.syncState()
 
-        // NavigationView 메뉴 아이템 클릭 리스너 설정
+        // NavigationView 메뉴 아이템 클릭 리스너 설정(공통 레이아웃 코드)
         binding.mainDrawerView.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
+                R.id.accommodation -> {
+                    startActivity(Intent(this, AccomActivity::class.java))
+                    true
+                }
+                R.id.restaurant -> {
+                    startActivity(Intent(this, ResActivity::class.java))
+                    true
+                }
+                R.id.tour -> {
+                    startActivity(Intent(this, TourActivity::class.java))
+                    true
+                }
+                R.id.festival -> {
+                    startActivity(Intent(this, FesActivity::class.java))
+                    true
+                }
+                R.id.shopping -> {
+                    startActivity(Intent(this, ShopActivity::class.java))
+                    true
+                }
                 R.id.community -> {
                     // '커뮤니티' 메뉴 아이템 클릭 시 CommReadActivity로 이동
                     startActivity(Intent(this, CommReadActivity::class.java))
                     true
                 }
-                // 다른 메뉴 아이템에 대한 처리 추가
+                R.id.chatting -> {
+                    startActivity(Intent(this, ChatActivity::class.java))
+                    true
+                }
 
                 else -> false
             }
@@ -195,6 +250,34 @@ class CommDetailActivity : AppCompatActivity() {
             startActivity(intent) //액티비티 열기
             overridePendingTransition(0, 0) //인텐트 효과 없애기
         }
+    } //onCreate
+
+    // 함수 구현 ---------------------------------------------------------------------------
+
+    // 툴바의 검색 뷰(공통 레이아웃)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu,menu)
+
+        // 검색 뷰에, 이벤트 추가하기.
+        val menuItem = menu?.findItem(R.id.menu_toolbar_search)
+        // menuItem 의 형을 SearchView 타입으로 변환, 형변환
+        val searchView = menuItem?.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(newText: String?): Boolean {
+                //검색어가 변경시 마다, 실행될 로직을 추가.
+                Log.d("kmk","텍스트 변경시 마다 호출 : ${newText} ")
+                return true
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // 검색어가 제출이 되었을 경우, 연결할 로직.
+                // 사용자 디비, 검색을하고, 그 결과 뷰를 출력하는 형태.
+                Toast.makeText(this@CommDetailActivity,"검색어가 전송됨 : ${query}", Toast.LENGTH_SHORT).show()
+                return true
+            }
+        })
+
+        return super.onCreateOptionsMenu(menu)
     }
 
     // 디테일 뷰에서 현재 로그인계정과 해당 글 작성자 일치여부에 따른 "수정","삭제"버튼 숨김여부 관련 코드
